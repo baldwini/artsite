3962ea7d685438f60901d962bee4ead9d31af13f
Ian Baldwin <mindofianb@gmail.com>
2022-08-31 13:18:26 -0400

    Added firestore basics and data service

---
+++ artsite/src/app/piece.model.ts
@@ -0,0 +1,10 @@
+export interface Piece{
+    name: string;
+    type: string;
+    medium: string;
+    series: string;
+    date: string;
+    size: string;
+    duration?: string;
+    price?: string;
+}
